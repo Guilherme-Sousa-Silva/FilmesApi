6dac64347115894a48ed7ae958e1b600dd2cb3d3
Guilherme de Sousa Silva <guilherme.sousa@interplayers.com.br>
2025-08-26 16:14:01 -0300

    Cria DTOs UpdateFilmeDto e ReadFilmeDto para operações de filmes • Adicionada a classe UpdateFilmeDto para atualização de dados de filmes, com validações. • Adicionada a classe ReadFilmeDto para retorno de dados de filmes nas respostas da API.
    Implementa métodos de edição e remoção de filmes, e adiciona mappings para UpdateFilmeDto e ReadFilmeDto
    • Criados os métodos na controller para editar (PUT) e deletar (DELETE) um filme.
    • Adicionados os DTOs UpdateFilmeDto e ReadFilmeDto.
    • Implementados os mappings no AutoMapper para UpdateFilmeDto e ReadFilmeDto.

--- FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/Controllers/FilmeController.cs
@@ -32,13 +32,48 @@ public class FilmeController : ControllerBase
     public async Task<IActionResult> ObterFilmes()
     {
         var filmes = await _context.Filmes.ToListAsync();
-        return Ok(filmes);
+        var filmesDto = _mapper.Map<List<ReadFilmeDto>>(filmes);
+        return Ok(filmesDto);
     }
 
     [HttpGet("{id}")]
-    public async Task<IActionResult> ObterFilmePorId([FromQuery] int id)
+    public async Task<IActionResult> ObterFilmePorId(int id)
     {
-        var filme = await _context.Filmes.FirstAsync(x => x.Id == id);
-        return Ok(filme);
+        var filme = await _context.Filmes.FirstOrDefaultAsync(x => x.Id == id);
+        if (filme == null)
+        {
+            return NotFound();
+        }
+
+        ReadFilmeDto filmeDto = _mapper.Map<ReadFilmeDto>(filme);
+        return Ok(filmeDto);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> AtualizarFilme([FromBody] UpdateFilmeDto updateFilmeDto)
+    {
+        var filme = await _context.Filmes.FirstOrDefaultAsync(x => x.Id == updateFilmeDto.Id);
+        if (filme == null)
+        {
+            return NotFound();
+        }
+
+        _mapper.Map(updateFilmeDto, filme);
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
+
+    [HttpDelete("id")]
+    public async Task<IActionResult> Deletar(int id)
+    {
+        var filme = await _context.Filmes.FirstOrDefaultAsync(x => x.Id == id);
+        if (filme == null)
+        {
+            return NotFound();
+        }
+
+        _context.Filmes.Remove(filme);
+        await _context.SaveChangesAsync();
+        return NoContent();
     }
 }
